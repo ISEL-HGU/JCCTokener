8fd659034a91e4f4693343d2ac51d03a9299c5c9
김동규 <kdg1941@naver.com>
2023-07-13 11:16:44 +0900

    create structure vector in SimpleName node

--- app/src/main/java/isel/csee/jcctokener/parser/ASTChecker.java
+++ app/src/main/java/isel/csee/jcctokener/parser/ASTChecker.java
@@ -29,43 +29,58 @@ public class ASTChecker extends ASTVisitor {
     private List<String> totalVariableList = new ArrayList<>();
     private List<String> argumentList = new ArrayList<>();
     private List<String> operatorList = new ArrayList<>();
+    private int[] tempVector;
     private List<int[]> structureVector = new ArrayList<>();
 
 
     @Override
     public boolean visit(ForStatement node) { // For Loop / 내부적으로 조건 및 본문 부분을 가져오는 것이 가능 -> Type 4, 5
-//        System.out.println("For init" + node.initializers());
-//        System.out.println("For condition: " + node.getExpression());
-//        System.out.println("For updater: " + node.updaters());
-//        System.out.println("For body: " + node.getBody());
-        System.out.println(NodeType.searchType(node));
+
         return super.visit(node);
     }
 
     @Override
     public boolean visit(ArrayAccess node) { // 배열에서 인덱스 부분 ex) a[1] -> [1] 이 부분 -> Type 6
-        System.out.println(NodeType.searchType(node));
+//        int[] vector = new int[30];
+//        ASTNode tempNode = node;
+//
+//        while(!(tempNode instanceof TypeDeclaration)) {
+//            vector = NodeType.searchType(tempNode, vector);
+//
+//            tempNode = tempNode.getParent();
+//        }
+//        System.out.println(node.getNodeType());
+//        System.out.println("");
+//        for(int i : vector) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println("");
+//
+//        structureVector.add(vector);
+
         return super.visit(node);
     }
 
     @Override
     public boolean visit(ThrowStatement node) { // Type 19
         System.out.println("Throw statement: " + node.getExpression());
-        System.out.println(NodeType.searchType(node));
+        int[] structureVector = new int[30];
+        structureVector = NodeType.searchType(node, structureVector);
         return super.visit(node);
     }
 
     @Override
-    public boolean visit(CatchClause node) { // Catch 부분을 가져올 수 있음 -> Type 21q
+    public boolean visit(CatchClause node) { // Catch 부분을 가져올 수 있음 -> Type 21
         System.out.println("Catch clause: " + node.getException());
-        System.out.println(NodeType.searchType(node));
+        int[] structureVector = new int[30];
+        structureVector = NodeType.searchType(node, structureVector);
         return super.visit(node);
     }
 
     @Override
     public boolean visit(SingleVariableDeclaration node) { // 확인 필요
         System.out.println("Single variable declaration: " + node.getName());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
@@ -79,7 +94,7 @@ public class ASTChecker extends ASTVisitor {
     public boolean visit(ArrayType node) { // 배열의 dimension / data type을 확인
         Type elementType = node.getElementType(); // data type 추출
         int dimensions = node.getDimensions(); // dimension 추출
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
@@ -91,8 +106,8 @@ public class ASTChecker extends ASTVisitor {
 
     @Override
     public boolean visit(ClassInstanceCreation node) { // Class to Instance / 24? 25?
-        System.out.println(NodeType.searchType(node));
-        System.out.println("Test");
+//        System.out.println(NodeType.searchType(node));
+//        System.out.println("Test");
         return super.visit(node);
     }
 
@@ -105,9 +120,9 @@ public class ASTChecker extends ASTVisitor {
 
     @Override
     public boolean visit(EnhancedForStatement node) { // Enhanced For Loop / type 4, 5
-        System.out.println("For condition: " + node.getExpression());
-        System.out.println("For body: " + node.getBody());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println("For condition: " + node.getExpression());
+//        System.out.println("For body: " + node.getBody());
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
@@ -122,7 +137,7 @@ public class ASTChecker extends ASTVisitor {
     public boolean visit(TryStatement node) { // 이 부분에서 Try-Catch-Finally block 모두 추출 가능 // Type 20
         Block tryBlock = node.getBody();
         for(Object statement : tryBlock.statements()) {
-            System.out.println(statement);
+//            System.out.println(statement);
             if (statement instanceof ExpressionStatement) {
                 ExpressionStatement expressionStatement = (ExpressionStatement) statement;
                 Expression expression = expressionStatement.getExpression(); // for loop 안의 statement = ExpressionStatement 먼저 취급 / 따라서 ExpressionStatement 만든 뒤 변환하는 과정을 거쳐야 한다
@@ -155,19 +170,19 @@ public class ASTChecker extends ASTVisitor {
                 }
             }
         }
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(ConstructorInvocation node) { // constructor 사용 부분 Type 24
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(LambdaExpression node) { // 람다식에 대한 처리 부분 -> 이 부분은 필요한지에 대해 더 생각 Type 23
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
@@ -194,35 +209,35 @@ public class ASTChecker extends ASTVisitor {
     }
     @Override
     public boolean visit(ArrayCreation node) { // Array 생성 부분 / Type 25
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(ReturnStatement node) { // return statement 사용 Type 12
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(TypeDeclaration node) { // Class 이름
-        System.out.println("Class name: " + node.getName().getIdentifier()); // 클래스인 경우에 name, identifier print
-        classList.add(node.getName().getIdentifier());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println("Class name: " + node.getName().getIdentifier()); // 클래스인 경우에 name, identifier print
+//        classList.add(node.getName().getIdentifier());
+//        System.out.println(NodeType.searchType(node));
 
         return super.visit(node);
     }
 
     @Override
     public boolean visit(FieldDeclaration node) { // Field Define -> Type에 추가해야함
-        System.out.println("Field name: " + node.fragments()); // Field 경우에 name, identifier print
-        System.out.println(NodeType.searchType(node));
+//        System.out.println("Field name: " + node.fragments()); // Field 경우에 name, identifier print
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(MethodDeclaration node) { // Method Definition Type 3
-        System.out.println("Method name: " + node.getName().getIdentifier());
+//        System.out.println("Method name: " + node.getName().getIdentifier());
 
         methodDeclarationList.add(node.getName().getIdentifier());
 
@@ -233,7 +248,7 @@ public class ASTChecker extends ASTVisitor {
 //        if(methodBody != null) {
 //            methodBody.accept(this);
 //        }
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
@@ -242,18 +257,18 @@ public class ASTChecker extends ASTVisitor {
         List<VariableDeclarationFragment> variableDeclarationFragmentList = node.fragments();
 
         variableList.add(variableDeclarationFragmentList.get(0).getName().toString());
-        System.out.println("Variable: " + variableDeclarationFragmentList.get(0).getName().toString());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println("Variable: " + variableDeclarationFragmentList.get(0).getName().toString());
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(MethodInvocation node) { // method 사용 부분 / 호출을 의미 -> Callee Type 11
-        System.out.println("Method Invocation: " + node.getName().getIdentifier());
-        methodInvocationList.add(node.getName().getIdentifier());
-        System.out.println(NodeType.searchType(node));
-
-        System.out.println("");
+//        System.out.println("Method Invocation: " + node.getName().getIdentifier());
+//        methodInvocationList.add(node.getName().getIdentifier());
+//        System.out.println(NodeType.searchType(node));
+//
+//        System.out.println("");
 
         ASTNode temp = node;
 
@@ -261,20 +276,20 @@ public class ASTChecker extends ASTVisitor {
 
         Arrays.fill(vector, 0);
 
-        for(int i = 0; i < vector.length; i++) {
-            System.out.print(vector[i]);
-        }
+//        System.out.println("");
 
         while(!(temp instanceof TypeDeclaration)) {
-            vector[NodeType.searchType(temp)] += 1;
-            System.out.println(ASTNode.nodeClassForType(temp.getNodeType()).getSimpleName());
+//            vector[NodeType.searchType(temp)] += 1;
+//            System.out.println(ASTNode.nodeClassForType(temp.getNodeType()).getSimpleName());
 
             temp = temp.getParent();
         }
 
-        for(int i = 0; i < vector.length; i++) {
-            System.out.print(vector[i]);
-        }
+//        for(int i = 0; i < vector.length; i++) {
+//            System.out.print(vector[i]);
+//        }
+
+//        System.out.println("");
 
 
         return super.visit(node);
@@ -283,30 +298,30 @@ public class ASTChecker extends ASTVisitor {
 
     @Override
     public boolean visit(InfixExpression node) { // Operator -> 이 method 내부에서 operator의 차이를 두어야 함 / Numeric, Logical, Condition etc. Type 7,8,9
-        System.out.println("Operator: " + node.getOperator().toString());
+//        System.out.println("Operator: " + node.getOperator().toString());
         operatorList.add(node.getOperator().toString());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
 
         ASTNode temp = node;
 
         int[] vector = new int[30];
         Arrays.fill(vector, 0);
-        System.out.println("");
+//        System.out.println("");
 
         while(!(temp instanceof TypeDeclaration)) {
-            vector[NodeType.searchType(temp)] += 1;
+//            vector[NodeType.searchType(temp)] += 1;
 //            System.out.println(NodeType.searchType(temp));
-            System.out.println(NodeType.searchType(temp));
-            System.out.println(ASTNode.nodeClassForType(temp.getNodeType()).getSimpleName());
+//            System.out.println(NodeType.searchType(temp));
+//            System.out.println(ASTNode.nodeClassForType(temp.getNodeType()).getSimpleName());
             temp = temp.getParent();
 
         }
 
         structureVector.add(vector);
 
-        for(int i = 0; i < vector.length; i++) {
-            System.out.print(vector[i]);
-        }
+//        for(int i = 0; i < vector.length; i++) {
+//            System.out.print(vector[i]);
+//        }
 
         return super.visit(node);
     }
@@ -314,23 +329,23 @@ public class ASTChecker extends ASTVisitor {
     @Override
     public boolean visit(SwitchCase node) { // switch case / switch 안에 존재하는 여러가지 case 각각을 의미 Type 14
         System.out.println("Switch Case: " + node.toString());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(SwitchStatement node) { // switch statement / switch 전체를 의미 Type 15
         System.out.println("Switch Statement: " + node.getExpression().toString());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
         return super.visit(node);
     }
 
     @Override
     public boolean visit(ExpressionStatement node) { // 잠깐 보류
         Expression expression = node.getExpression();
-        System.out.println(expression.toString());
+//        System.out.println(expression.toString());
         totalVariableList.add(expression.toString());
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
 
         return super.visit(node);
     }
@@ -339,21 +354,42 @@ public class ASTChecker extends ASTVisitor {
     @Override
     public boolean visit(Assignment node) { // Type 10
         Expression leftHandSide = node.getLeftHandSide();
-        System.out.println("Left side: " + leftHandSide);
+//        System.out.println("Left side: " + leftHandSide);
         totalVariableList.add(leftHandSide.toString());
 
         Expression rightHandSide = node.getRightHandSide();
-        System.out.println("Right side: " + rightHandSide);
+//        System.out.println("Right side: " + rightHandSide);
 
-        System.out.println(NodeType.searchType(node));
+//        System.out.println(NodeType.searchType(node));
 
         return super.visit(node);
     }
 
     @Override
     public boolean visit(SimpleName node) { // 보류
-//        System.out.println("Simple name: " + node.getIdentifier());
-        System.out.println(NodeType.searchType(node));
+        System.out.println("Simple name: " + node.getIdentifier());
+
+        int[] vector = new int[30];
+        ASTNode tempNode = node;
+//        System.out.println(ASTNode.nodeClassForType(node.getParent().getNodeType()).getSimpleName());
+
+        if(!(node.getParent() instanceof PackageDeclaration)) {
+            while(tempNode != null && !(tempNode instanceof TypeDeclaration) && !(tempNode instanceof PackageDeclaration)) {
+                vector = NodeType.searchType(tempNode, vector);
+
+                tempNode = tempNode.getParent();
+            }
+
+//        System.out.println("");
+            for(int i : vector) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+
+            structureVector.add(vector);
+        }
+
+
 
         return super.visit(node);
     }
--- app/src/main/java/isel/csee/jcctokener/types/NodeType.java
+++ app/src/main/java/isel/csee/jcctokener/types/NodeType.java
@@ -3,138 +3,170 @@ package isel.csee.jcctokener.types;
 import org.eclipse.jdt.core.dom.*;
 
 public class NodeType {
-    public static int searchType(ASTNode astNode) {
+    public static int[] searchType(ASTNode astNode, int[] arr) {
         if(astNode instanceof IfStatement && ((IfStatement) astNode).getExpression() == astNode) { // Type 1 (If Condition)
-            return 1;
+            arr[1] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof IfStatement && ((IfStatement) astNode.getParent()).getThenStatement() == astNode) { // type 2 (If Body)
-            return 2;
+            arr[2] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof IfStatement && ((IfStatement) astNode.getParent()).getElseStatement() == astNode) { // type 2 (Else Body)
-            return 2;
+            arr[2] += 1;
+            return arr;
         }
 
         if(astNode instanceof MethodDeclaration && ((MethodDeclaration) astNode).isConstructor() == false) { // type 3 (Method Definition)
-            return 3;
+            arr[3] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof ForStatement && ((ForStatement) astNode.getParent()).getExpression() == astNode) { // type 4 (For Loop Condition)
-            return 4;
+            arr[4] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof EnhancedForStatement && ((EnhancedForStatement) astNode.getParent()).getExpression() == astNode) { // type 4 (Enhanced For Loop Condition)
-            return 4;
+            arr[4] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof WhileStatement && ((WhileStatement) astNode.getParent()).getExpression() == astNode) { // type 4 (While Loop Condition)
-            return 4;
+            arr[4] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof ForStatement && ((ForStatement) astNode.getParent()).getBody() == astNode) { // type 5 (For Loop Body)
-            return 5;
+            arr[5] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof EnhancedForStatement && ((EnhancedForStatement) astNode.getParent()).getBody() == astNode) { // type 5 (Enhanced For Loop Body)
-            return 5;
+            arr[5] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof WhileStatement && ((WhileStatement) astNode.getParent()).getBody() == astNode) { // type 5 (While Loop Body)
-            return 5;
+            arr[5] += 1;
+            return arr;
         }
 
         if(astNode instanceof ArrayAccess) { // type 6 (Array Selector)
-            return 6;
+            arr[6] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.EQUALS) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.NOT_EQUALS) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.GREATER) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.LESS) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.LESS_EQUALS) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.GREATER_EQUALS) { // type 7 (Logical Expression)
-            return 7;
+            arr[7] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.AND) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.DIVIDE) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.LEFT_SHIFT) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.RIGHT_SHIFT_SIGNED) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.RIGHT_SHIFT_UNSIGNED) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.MINUS) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.OR) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.PLUS) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.REMAINDER) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.TIMES) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.XOR) { // type 8 (Numeric Expression)
-            return 8;
+            arr[8] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.CONDITIONAL_AND) { // type 9 (Condition Expression)
-            return 9;
+            arr[9] += 1;
+            return arr;
         }
 
         if(astNode instanceof InfixExpression && ((InfixExpression) astNode).getOperator() == InfixExpression.Operator.CONDITIONAL_OR) { // type 9 (Condition Expression)
-            return 9;
+            arr[9] += 1;
+            return arr;
         }
 
         if(astNode instanceof Assignment) { // type 10 (Assignment Expression)
-
-            return 10;
+            arr[10] += 1;
+            return arr;
         }
 
         if(astNode instanceof MethodInvocation) { // type 11 (Method Invocation)
-            return 11;
+            arr[11] += 1;
+            return arr;
         }
 
         if(astNode instanceof ReturnStatement) { // type 12 (Return Statement)
-            return 12;
+            arr[12] += 1;
+            return arr;
         }
 
 //        if(astNode instanceof SwitchCase) { // type 13 (Switch Case Body)
@@ -142,57 +174,70 @@ public class NodeType {
 //        }
 
         if(astNode instanceof SwitchStatement) { // type 14 (Switch Body)
-            return 14;
+            arr[14] += 1;
+            return arr;
         }
 
         if(astNode instanceof SwitchExpression) { // type 15 (Switch Condition)
-            return 15;
+            arr[15] += 1;
+            return arr;
         }
 
         if(astNode instanceof VariableDeclarationExpression) { // type 16 (Variable Declaration)
-            return 16;
+            arr[16] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof AssertStatement && ((AssertStatement) astNode.getParent()).getExpression() == astNode) { // type 17 (Assert Condition)
-            return 17;
+            arr[17] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof AssertStatement && ((AssertStatement) astNode.getParent()).getMessage() == astNode) { // type 18 (Assert Body)
-            return 18;
+            arr[18] += 1;
+            return arr;
         }
 
         if(astNode instanceof ThrowStatement) { // type 19 (Throw Body)
-            return 19;
+            arr[19] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof TryStatement && ((TryStatement) astNode.getParent()).getBody() == astNode) { // type 20 (Try Body)
-            return 20;
+            arr[20] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof CatchClause && ((CatchClause) astNode.getParent()).getBody() == astNode) { // type 21 (Catch Body)
-            return 21;
+            arr[21] += 1;
+            return arr;
         }
 
         if(astNode.getParent() instanceof TryStatement && ((TryStatement) astNode.getParent()).getFinally() == astNode) { // type 22 (Finally Body)
-            return 22;
+            arr[22] += 1;
+            return arr;
         }
 
         if(astNode instanceof LambdaExpression) { // type 23 (Lambda Expression)
-            return 23;
+            arr[23] += 1;
+            return arr;
         }
 
         if(astNode instanceof ConstructorInvocation) { // type 24 (Constructor Invocation)
-            return 24;
+            arr[24] += 1;
+            return arr;
         }
 
         if(astNode instanceof MethodDeclaration && ((MethodDeclaration) astNode).isConstructor() == true) { // type 25 (Class Creator)
-            return 25;
+            arr[25] += 1;
+            return arr;
         }
 
         if(astNode instanceof ArrayCreation) { // type 25 (Array Creator)
-            return 25;
+            arr[25] += 1;
+            return arr;
         }
 
-        return 0;
+        return arr;
     }
 }
